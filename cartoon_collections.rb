68b0fa9079b59756a0ebe1463689ede971b2a004
William West <wwest466@gmail.com>
2020-08-25 14:24:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(arr)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map {|name| name.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
